d355ad423cab0b07b06cce5d38eae08ff2078ed7
Corben-Smith <corbensmith782@gmail.com>
2025-03-27 15:24:10 -0400

    code

--- scripts/Collectables/Status_Collectable.gd
+++ scripts/Collectables/Status_Collectable.gd
@@ -4,19 +4,19 @@ class_name Status_Collectable
 @export var statuses: Array[StatusEffect] = [] 
 
 func _ready():
-    if !statuses or len(statuses):
-        push_error("provide a status effect resource")
+	if !statuses or len(statuses):
+		push_error("provide a status effect resource")
 
-    connect("body_entered", _on_body_entered)
-    connect("body_exited", _on_body_exited)
+	connect("body_entered", _on_body_entered)
+	connect("body_exited", _on_body_exited)
 
 func _on_body_entered(body: Node2D):
-    if body is Player:
-        _collect(body)
-        queue_free()
+	if body is Player:
+		_collect(body)
+		queue_free()
 
 func _collect(player: Player):
-    if player.status_manager:
-        for status in statuses:
-            var new_status = status.duplicate_deep()
-            player.status_manager.apply_effect(new_status, player)
+	if player.status_manager:
+		for status in statuses:
+			var new_status = status.duplicate_deep()
+			player.status_manager.apply_effect(new_status, player)
