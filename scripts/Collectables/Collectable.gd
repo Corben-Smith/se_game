b78bf54ff3f2ba68beb1f92fdecfc2658511ae96
Corben-Smith <corbensmith782@gmail.com>
2025-04-08 12:25:35 -0400

    changes

--- scripts/Collectables/Collectable.gd
+++ scripts/Collectables/Collectable.gd
@@ -2,14 +2,14 @@ extends Area2D
 class_name Collectable
 
 func _ready():
-    connect("body_entered", _on_body_entered)
-    connect("body_exited", _on_body_exited)
+	connect("body_entered", _on_body_entered)
+	connect("body_exited", _on_body_exited)
 
 func _on_body_exited(body: Node2D):
-    pass
+	pass
 
 func _on_body_entered(body: Node2D):
-    pass
+	pass
 
 func _collect(player: Player):
-    pass
+	pass
