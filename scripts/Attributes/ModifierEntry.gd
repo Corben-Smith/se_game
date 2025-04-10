cd39cb61506fafd5cac60734f13fc77f88cbca8d
Corben-Smith <corbensmith782@gmail.com>
2025-04-10 12:01:30 -0400

    finished

--- scripts/Attributes/ModifierEntry.gd
+++ scripts/Attributes/ModifierEntry.gd
@@ -2,4 +2,12 @@ extends Resource
 class_name ModifierEntry
 
 @export var stat_name: String = ""
-@export var modifier: Modifier = null
+@export var modifier_type: Modifier.Type = Modifier.Type.ADDITIVE
+@export var modifier_value: float = 0.0
+
+var modifier: Modifier = null
+
+func get_modifier() -> Modifier:
+    if modifier == null:
+        modifier = Modifier.new(modifier_type, modifier_value)
+    return modifier
