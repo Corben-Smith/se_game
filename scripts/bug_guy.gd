e10720ce5d22f79e6c79c83f7e371eddfd9c629a
TheBoiCaf <144686110+TheBoiCaf@users.noreply.github.com>
2025-05-05 14:58:48 -0400

    maybe fixed

--- scripts/bug_guy.gd
+++ scripts/bug_guy.gd
@@ -36,6 +36,20 @@ func _process(delta):
         animated_sprite.flip_h = false
 	
 	if ray_cast_left.is_colliding() and !ray_cast_left.get_collider().is_in_group("player"):
+		direction = 1
+		animated_sprite.flip_h = true
+		
+	if ray_cast_right.is_colliding() and !ray_cast_right.get_collider().is_in_group("player"):
+		direction = -1
+		animated_sprite.flip_h = false
+		
+	if not ray_cast_dl.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = true
+	
+	if not ray_cast_dr.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = false
 
     velocity.x = direction * SPEED * delta
     if !is_on_floor():
