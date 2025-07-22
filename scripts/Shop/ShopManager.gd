c8c2e52cbf7006b2832d3f8e56ad29bf59d75bf1
Gabriel Palma <144812765+GabrielP012@users.noreply.github.com>
2025-07-22 14:05:15 -0400

    Added Level 1
    Added level 1, key, door, and modified "ShopManager", which stores the player's inventory.

--- scripts/Shop/ShopManager.gd
+++ scripts/Shop/ShopManager.gd
@@ -8,15 +8,28 @@ var exp = 100
 	preload("res://scripts/Items/LevelUpElixir.tres")
 ]
 
-
+# Player Items Inventory
 var inventory: Array[InventoryItem] = []
+# Player Key Inventory
+var key_inventory: Array[String] = []
 
+# Default Items when the player comes to life
 func _ready():
 	var healing_potion = InventoryItem.new()
 	healing_potion.item = item_list[0]  # HealingPotion.tres
 	healing_potion.count = 1
 	inventory.append(healing_potion)
 
+# Key logic
+func add_key(key_id: String):
+	if key_id not in key_inventory:
+		key_inventory.append(key_id)
+		print("Key added to inventory:", key_id)
+
+func has_key(key_id: String) -> bool:
+	return key_id in key_inventory
+
+# Items for sale in the shop
 var items = {
 	0: {
 		"Name": "Healing Potion",
