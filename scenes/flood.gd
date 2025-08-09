6bba53e1e40b046230e4f0d104d5c67875855f7d
corbensmith <corbensmith782@gmail.com>
2025-08-09 12:24:01 -0400

    flood fix

--- scenes/flood.gd
+++ scenes/flood.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @export var rise_speed: float = 20.0
-@export var stop_y: float = -300.0  # Adjust this to where you want the water to stop
+@export var stop_y: Node2D
 @export var delay_before_rising: float = 3.0
 
 var rising := false
@@ -9,6 +9,8 @@ var time_passed := 0.0
 var start_position: Vector2
 
 func _ready():
+	if !stop_y:
+		push_error("nostopy")
 	start_position = global_position
 	connect("body_entered", _on_body_entered)
 
@@ -17,7 +19,7 @@ func _process(delta):
 		time_passed += delta
 		if time_passed >= delay_before_rising:
 			rising = true
-	elif global_position.y > stop_y:
+	elif global_position.y > stop_y.global_position.y:
 		global_position.y -= rise_speed * delta
 
 func _on_body_entered(body):
